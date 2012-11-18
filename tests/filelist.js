cbe45aa15aad950e9c195ba3126e0948c6eb4d71
Garen Torikian <gjtorikian@gmail.com>
2012-11-17 19:11:11 -0800

    Implement all the speedups

---
+++ tests/filelist.js
@@ -0,0 +1,52 @@
+"mocha";
+
+var Assert = require("assert");
+var Exec = require("child_process").exec;
+
+var basePath = __dirname + "/filelist_fixtures";
+
+var nakPath = "node ../bin/nak";
+var options1 = [
+        "-l",
+        "-H",
+        "-p ../.agignore",
+        basePath
+    ],
+    options2 = [
+        "-l",
+        "-p ../.agignore",
+        basePath
+    ];
+
+describe("filelist", function() {
+    it("should get filelist, including hidden files and binaries",  function(next) {
+       Exec(nakPath + " " + options1.join(" "), function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.error(err);
+            console.error(stderr);
+        }
+        var files = stdout.split("\n").filter(function(file) { return !!file; }).sort();
+
+        Assert.equal(files[2], basePath + "/level1/Toasty.gif");
+        Assert.equal(files[3], basePath + "/level1/level2/.hidden");
+        Assert.equal(files[4], basePath + "/level1/level2/.level3a/.hidden");
+
+        next();
+       });
+    });
+
+    it("should get filelist, without hidden files",  function(next) {
+       Exec(nakPath + " " + options2.join(" "), function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.error(err);
+            console.error(stderr);
+        }
+        var files = stdout.split("\n").filter(function(file) { return !!file; }).sort();
+            
+        Assert.equal(files[3], basePath + "/level1/level2/level2.rb");
+        Assert.equal(files[4], basePath + "/level1/level2/level3/level4/level4.txt");
+
+        next();
+       });
+    });
+});
---
+++ tests/search.js
@@ -0,0 +1,197 @@
+"mocha";
+
+var Assert = require("assert");
+var Exec = require("child_process").exec;
+
+var basePath = __dirname + "/search_fixtures";
+
+var nakPath = "node ../bin/nak";
+
+var options1 = [
+        "-p ../.agignore",
+        "-i",
+        "-q",
+        "'sriracha'", 
+        basePath
+    ],
+    options2 = [
+        "-p ../.agignore",
+        "-q",
+        "'Messenger'",
+        basePath
+    ],
+    options3 = [
+        "-p ../.agignore",
+        "-q",
+        "-w",
+        "'gastro'",
+        basePath
+    ],
+    options4 = [
+        "-p ../.agignore",
+        "-i",
+        "'pb.'",
+        basePath
+    ],
+    options5 = [
+        "-p ../.agignore",
+        "-H",
+        "'.+wave'",
+        basePath
+    ],
+    options6 = [
+        "-p ../.agignore",
+        "-G '*.txt, file*.gif'",
+        "-i",
+        "'shorts'",
+        basePath
+    ],
+    options7 = [
+        "-p ../.agignore",
+        "--ignore 'file*.txt'",
+        "'williamsburg'",
+        "-H",
+        basePath
+    ];
+    
+describe("search", function() {
+    it("should find matches without regexp, case-sensitive OFF and word boundaries OFF",  function(next) {
+       Exec(nakPath + " " + options1.join(" "), function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.error(err);
+            console.error(stderr);
+        }
+        
+        var lines = stdout.split("\n");
+        var msgLine = lines[lines.length - 2].split(" "); 
+        var count = msgLine[1];
+        var filecount = msgLine[4];
+
+        Assert.equal(count, 8);
+        Assert.equal(filecount, 4);
+        Assert.equal(lines.length, 16);
+
+        next();
+       });
+    });
+
+    it("should find matches without regexp, case-sensitive ON and word boundaries OFF",  function(next) {
+       Exec(nakPath + " " + options2.join(" "), function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.error(err);
+            console.error(stderr);
+        }
+        
+        var lines = stdout.split("\n");
+        var msgLine = lines[lines.length - 2].split(" "); 
+        var count = msgLine[1];
+        var filecount = msgLine[4];
+
+        Assert.equal(count, 2);
+        Assert.equal(filecount, 2);
+        Assert.equal(lines.length, 8);
+
+        next();
+       });
+    });
+
+    it("should find matches without regexp, case-sensitive OFF and word boundaries ON",  function(next) {
+       Exec(nakPath + " " + options3.join(" "), function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.error(err);
+            console.error(stderr);
+        }
+       
+        var lines = stdout.split("\n");
+        var msgLine = lines[lines.length - 2].split(" "); 
+        var count = msgLine[1];
+        var filecount = msgLine[4];
+
+        Assert.equal(count, 3);
+        Assert.equal(filecount, 3);
+        Assert.equal(lines.length, 11);
+
+        next();
+       });
+    });
+
+    it("should find matches with a regexp, case-sensitive OFF",  function(next) {
+       Exec(nakPath + " " + options4.join(" "), function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.error(err);
+            console.error(stderr);
+        }
+        
+        var lines = stdout.split("\n");
+        var msgLine = lines[lines.length - 2].split(" "); 
+        var count = msgLine[1];
+        var filecount = msgLine[4];
+
+        Assert.equal(count, 8);
+        Assert.equal(filecount, 4);
+        Assert.equal(lines.length, 18);
+
+        next();
+       });
+    });
+
+    it("should find matches with a regexp, case-sensitive ON, including the default .agignore file, and hidden files",  function(next) {
+       Exec(nakPath + " " + options5.join(" "), function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.error(err);
+            console.error(stderr);
+        }
+
+        var lines = stdout.split("\n");
+        var msgLine = lines[lines.length - 2].split(" "); 
+        var count = msgLine[1];
+        var filecount = msgLine[4];
+
+        Assert.equal(count, 14);
+        Assert.equal(filecount, 7);
+        Assert.equal(lines.length, 30);
+
+        next();
+       });
+    });
+   
+    it("should find matches without case-sensitive regexp, only two file types, and no hidden files (even if they contain the string)",  function(next) {
+       Exec(nakPath + " " + options6.join(" "), function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.error(err);
+            console.error(stderr);
+        }
+
+        var lines = stdout.split("\n");
+        var msgLine = lines[lines.length - 2].split(" "); 
+        var count = msgLine[1];
+        var filecount = msgLine[4];
+
+        Assert.equal(count, 2);
+        Assert.equal(filecount, 2);
+        Assert.equal(lines.length, 8);
+
+        next();
+       });
+    });
+
+    it("should find matches without regexp, excluding txt files and including hidden ones",  function(next) {
+       Exec(nakPath + " " + options7.join(" "), function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.error(err);
+            console.error(stderr);
+        }
+
+        var lines = stdout.split("\n");
+        var msgLine = lines[lines.length - 2].split(" "); 
+        var count = msgLine[1];
+        var filecount = msgLine[4];
+
+        Assert.equal(count, 14);
+        Assert.equal(filecount, 4);
+        Assert.equal(lines.length, 20);
+
+        next();
+       });
+    });
+});
