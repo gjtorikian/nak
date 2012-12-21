9bae02f51e896dc4356d3ed72516e7f93784bcaa
Garen Torikian <gjtorikian@gmail.com>
2012-12-21 02:36:03 -0800

    Implement new and proper sorting

---
+++ tests/sorting.js
@@ -0,0 +1,45 @@
+"mocha";
+
+var Assert = require("assert");
+var Exec = require("child_process").exec;
+
+var basePath = __dirname + "/sorting_fixtures";
+
+var nakPath = "node ../bin/nak";
+//var nakPath = "node ../build/nak.min";
+
+var options = [
+        "-a ../.nakignore",
+        "-l", 
+        basePath
+    ];
+    
+describe("search", function() {
+    it("should return directory results properly",  function(next) {
+       Exec(nakPath + " " + options.join(" "), function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.error(err);
+            console.error(stderr);
+        }
+
+        var fpath = options[2];
+        var lines = stdout.split("\n");
+
+        var expected = "a.txt\n" +
+                       "p.txt\n" +
+                       "README.md\n" +
+                       "n_folder/BBB.txt\n" +
+                       "n_folder/z.txt\n" +
+                       "n_folder/ZZZ.txt\n" +
+                       "n_folder/www/a.txt\n" +
+                       "p_folder/a.txt\n" +
+                       "p_folder/c.txt\n" +
+                       "p_folder/b_folder/l.txt\n" + '';
+        
+        Assert.equal(lines.length, 11);
+        Assert.equal(stdout.replace(new RegExp(fpath + "/", "g"), ""), expected);
+
+        next();
+       });
+    });
+});
---
+++ tests/sorting_fixtures/README.md
@@ -0,0 +1,3 @@
+Tests for sorts. 
+
+Basically, subdirectories should only be traversed after the parent directory's contents have been. It's a tricky problem to solve in merge sort!
