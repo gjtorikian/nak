3918ec6803d36eb49a8d5a9711a64bf6938c869b
Ben Ogle <ogle.ben@gmail.com>
2013-09-13 16:16:19 -0700

    Cleanup search tests.

--- tests/search_test.js
+++ tests/search_test.js
@@ -10,22 +10,31 @@ var nak = require('../lib/nak');
 var nakPath = "node bin/nak";
 //var nakPath = "node build/nak.min";
 
+parseOutput = function(err, stdout, stderr) {
+  if (err || stderr) {
+    console.error(err);
+    console.error(stderr);
+  }
+
+  var lines = stdout.split("\n");
+  var msgLine = lines[lines.length - 2].split(" ");
+
+  return {
+    lines: lines,
+    msgLine: msgLine,
+    count: msgLine[1],
+    filecount: msgLine[4]
+  };
+};
+
 describe("search", function() {
     it("should find matches without regexp, case-sensitive OFF and word boundaries OFF",  function(next) {
        Exec(nakPath + " " + "-a .nakignore -i -q 'sriracha' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 8);
-        Assert.equal(filecount, 4);
-        Assert.equal(lines.length, 16);
+        Assert.equal(output.count, 8);
+        Assert.equal(output.filecount, 4);
+        Assert.equal(output.lines.length, 16);
 
         next();
        });
@@ -33,19 +42,11 @@ describe("search", function() {
 
     it("should find matches without regexp, follow ON, case-sensitive OFF and word boundaries OFF",  function(next) {
        Exec(nakPath + " " + "-f -a .nakignore -i -q 'sriracha' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 9);
-        Assert.equal(filecount, 5);
-        Assert.equal(lines.length, 19);
+        Assert.equal(output.count, 9);
+        Assert.equal(output.filecount, 5);
+        Assert.equal(output.lines.length, 19);
 
         next();
        });
@@ -53,19 +54,11 @@ describe("search", function() {
 
     it("should find matches without regexp, case-sensitive ON and word boundaries OFF",  function(next) {
        Exec(nakPath + " " + "-a .nakignore -q 'Messenger' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 2);
-        Assert.equal(filecount, 2);
-        Assert.equal(lines.length, 8);
+        Assert.equal(output.count, 2);
+        Assert.equal(output.filecount, 2);
+        Assert.equal(output.lines.length, 8);
 
         next();
        });
@@ -73,19 +66,11 @@ describe("search", function() {
 
     it("should find matches without regexp, case-sensitive OFF and word boundaries ON",  function(next) {
        Exec(nakPath + " " + "-a .nakignore -q -w 'gastro' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 3);
-        Assert.equal(filecount, 3);
-        Assert.equal(lines.length, 11);
+        Assert.equal(output.count, 3);
+        Assert.equal(output.filecount, 3);
+        Assert.equal(output.lines.length, 11);
 
         next();
        });
@@ -93,19 +78,11 @@ describe("search", function() {
 
     it("should find matches with a regexp, case-sensitive OFF",  function(next) {
        Exec(nakPath + " " + "-a .nakignore -i 'pb.' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 8);
-        Assert.equal(filecount, 4);
-        Assert.equal(lines.length, 18);
+        Assert.equal(output.count, 8);
+        Assert.equal(output.filecount, 4);
+        Assert.equal(output.lines.length, 18);
 
         next();
        });
@@ -113,19 +90,11 @@ describe("search", function() {
 
     it("should find matches with a regexp, case-sensitive ON, including the default .nakignore file, and hidden files",  function(next) {
        Exec(nakPath + " " + "-a .nakignore -H '.+wave' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 17);
-        Assert.equal(filecount, 10);
-        Assert.equal(lines.length, 39);
+        Assert.equal(output.count, 17);
+        Assert.equal(output.filecount, 10);
+        Assert.equal(output.lines.length, 39);
 
         next();
        });
@@ -133,19 +102,11 @@ describe("search", function() {
 
     it("should find matches without case-sensitive regexp, only two file types, and no hidden files (even if they contain the string)",  function(next) {
        Exec(nakPath + " " + "-a .nakignore -G '*.txt, file*.gif' -i 'shorts' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 2);
-        Assert.equal(filecount, 2);
-        Assert.equal(lines.length, 8);
+        Assert.equal(output.count, 2);
+        Assert.equal(output.filecount, 2);
+        Assert.equal(output.lines.length, 8);
 
         next();
        });
@@ -153,19 +114,11 @@ describe("search", function() {
 
     it("should find matches without regexp, excluding txt files and including hidden ones",  function(next) {
        Exec(nakPath + " " + "-a .nakignore --ignore 'file*.txt' 'williamsburg' -H " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 15);
-        Assert.equal(filecount, 5);
-        Assert.equal(lines.length, 23);
+        Assert.equal(output.count, 15);
+        Assert.equal(output.filecount, 5);
+        Assert.equal(output.lines.length, 23);
 
         next();
        });
@@ -173,14 +126,9 @@ describe("search", function() {
 
     it("should report multiple matches in same line using ackmate",  function(next) {
        Exec(nakPath + " " + "-a .nakignore --ackmate 'wolf' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
-        
-        var lines = stdout.split("\n");
+        var output = parseOutput(err, stdout, stderr);
 
-        Assert.equal("8;26 4,42 4", lines[4].split(":")[0]);
+        Assert.equal("8;26 4,42 4", output.lines[4].split(":")[0]);
 
         next();
        });
@@ -189,19 +137,11 @@ describe("search", function() {
     // see: http://me.dt.in.th/page/StringDecoder
     it("should find incomplete thai characters",  function(next) {
        Exec(nakPath + " " + "-a .nakignore 'ร' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
-        var lines = stdout.split("\n");
+        var output = parseOutput(err, stdout, stderr);
 
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 1);
-        Assert.equal(filecount, 1);
-        Assert.equal(lines.length, 5);
+        Assert.equal(output.count, 1);
+        Assert.equal(output.filecount, 1);
+        Assert.equal(output.lines.length, 5);
 
         next();
        });
@@ -209,19 +149,11 @@ describe("search", function() {
 
     it("should find matches without regexp, in a starting hidden dir",  function(next) {
        Exec(nakPath + " " + "-f -a .nakignore -i -q 'sriracha' " + basePath + "/.root", function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 4);
-        Assert.equal(filecount, 2);
-        Assert.equal(lines.length, 8);
+        Assert.equal(output.count, 4);
+        Assert.equal(output.filecount, 2);
+        Assert.equal(output.lines.length, 8);
 
         next();
        });
@@ -229,18 +161,10 @@ describe("search", function() {
 
     it("should not find matches in secondary directories, while ignoring VCS",  function(next) {
        Exec(nakPath + " " + "-f -a .nakignore --addVCSIgnores -i -q 'farn' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-        
-        Assert.equal(count, 0);
-        Assert.equal(filecount, 0);
+        Assert.equal(output.count, 0);
+        Assert.equal(output.filecount, 0);
 
         next();
        });
@@ -251,19 +175,10 @@ describe("search", function() {
        var fn = 'if (/file1\.txt/.test(filepath)) return "photo";\nreturn null;';
 
        Exec(nakPath + " " + "-a .nakignore 'photo' --onFilepathSearchFn '" + fn + "' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 5);
-        Assert.equal(filecount, 3);
+        Assert.equal(output.count, 5);
+        Assert.equal(output.filecount, 3);
 
         next();
        });
@@ -278,19 +193,10 @@ describe("search", function() {
        process.env.nak_onFilepathSearchFn = nak.serialize(fn);
 
        Exec(nakPath + " " + "-a .nakignore 'photo' " + basePath, function(err, stdout, stderr) {
-        if (err || stderr) {
-            console.error(err);
-            console.error(stderr);
-        }
+        var output = parseOutput(err, stdout, stderr);
 
-        var lines = stdout.split("\n");
-
-        var msgLine = lines[lines.length - 2].split(" ");
-        var count = msgLine[1];
-        var filecount = msgLine[4];
-
-        Assert.equal(count, 5);
-        Assert.equal(filecount, 3);
+        Assert.equal(output.count, 5);
+        Assert.equal(output.filecount, 3);
         next();
        });
     });
